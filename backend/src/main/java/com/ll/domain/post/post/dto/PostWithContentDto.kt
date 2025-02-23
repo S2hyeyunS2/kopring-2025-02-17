e2ca66a5439b3cd29404e0694bb945e11295dbae
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-02-23 23:20:36 +0900

    Empty.java, PageDto.java kotlin 전환

--- backend/src/main/java/com/ll/domain/post/post/dto/PostWithContentDto.kt
+++ backend/src/main/java/com/ll/domain/post/post/dto/PostWithContentDto.kt
@@ -1,15 +1,15 @@
-package com.ll.domain.post.post.dto;
+package com.ll.domain.post.post.dto
 
 import com.ll.domain.post.post.entity.Post
 
-
 class PostWithContentDto(
-    val content:String,
-    val actorCanModify:Boolean?=null,
-    val actorCanDelete:Boolean?=null,
-    private val post: Post):PostDto(post) {
+    val content: String,
+    var actorCanModify: Boolean? = null,
+    var actorCanDelete: Boolean? = null,
+    private val post: Post
+) : PostDto(post) {
     constructor(post: Post) : this(
         content = post.content,
-        post = post,
+        post = post
     )
 }
